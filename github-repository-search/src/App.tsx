ddfb960bcf012df57b174c82d63e9ac8b6c0a5d8
KonstantinGarbers <wurstm162@gmail.com>
2023-11-13 15:11:31 +0800

    FInished sidebar component html

--- github-repository-search/src/App.tsx
+++ github-repository-search/src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content/Content";
+import Seperator from "./shared/Seperator";
 
 function App() {
   return (
-    <main>
+    <>
       <Header />
-      <Sidebar />
-    </main>
+      <main className="grid grid-cols-4 gap-0 pt-4 px-4">
+        <div className="col-span-1 flex items-center">
+          <>
+            <Sidebar />
+            <Seperator />
+          </>
+        </div>
+        <div className="col-span-3">
+          <Content />
+        </div>
+      </main>
+    </>
   );
 }
 
--- github-repository-search/src/components/Sidebar.tsx
+++ github-repository-search/src/components/Sidebar.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+
+const profilePictureUrl = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D";
+const name = "Konstantin Garbers"
+const username = "garbersKonstantin"
+const description = "Software developer with a passion for software, living in Munich but currently in Taiwan for an exchange semester"
 
 function Sidebar() {
   return (
-    <div>
-        This is a header
+    <div className="flex flex-col items-stretch">
+      <img
+        loading="lazy"
+        srcSet={profilePictureUrl}
+        alt="Profile Picture"
+      />
+      <h1 className="text-black text-4xl font-medium w-full -mr-5 mt-3 max-md:mt-10">
+        {name}
+      </h1>
+      <h2 className="text-black text-opacity-50 text-2xl w-full -mr-3">
+        {username}
+      </h2>
+      <p className="text-black text-xl w-full -mr-5 mt-6">
+        {description}
+      </p>
+      <div className="w-px h-full bg-gray-300 -mr-5"></div>
     </div>
   );
 }
