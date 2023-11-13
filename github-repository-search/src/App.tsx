6f36e2afdf8a7e4ec625a84aee27df728100bb10
KonstantinGarbers <wurstm162@gmail.com>
2023-11-13 15:39:20 +0800

    Added conditional rendering to seperator

--- github-repository-search/src/App.tsx
+++ github-repository-search/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <div className="col-span-1 flex items-center">
           <>
             <Sidebar />
-            <Seperator />
+            <Seperator vertical={true}/>
           </>
         </div>
         <div className="col-span-3">
--- github-repository-search/src/shared/Seperator.tsx
+++ github-repository-search/src/shared/Seperator.tsx
@@ -1,6 +1,14 @@
-function Seperator() {
+import React from 'react';
+
+interface SeperatorProps {
+  vertical: boolean;
+}
+
+function Seperator( {vertical} : SeperatorProps ) {
+  const style = vertical ? "h-full w-px mx-2" : "w-full h-px my-2"; 
+
   return (
-    <div className="bg-gray-400 w-px h-full mx-2"></div>
+    <div className={"bg-gray-400 " + style}></div>
   );
 }
 
