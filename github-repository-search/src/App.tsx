a4e4acc9645c091ad43137083e93dfc07fed8c7a
KonstantinGarbers <wurstm162@gmail.com>
2023-11-14 22:37:52 +0800

    Added query

--- github-repository-search/src/App.tsx
+++ github-repository-search/src/App.tsx
@@ -1,10 +1,47 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import Content from "./components/Content/Content";
+import Content from "./components/content/Content";
 import Seperator from "./shared/Seperator";
+import {
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function App() {
+
+  const GET_GITHUB = gql`
+  query GetGithub {
+    user(login: "AlexPerathoner") {
+      name
+      bioHTML
+      repositories(first: 100 orderBy:{
+        field:STARGAZERS
+        direction:DESC
+      }) {
+        edges {
+          node {
+            name
+            descriptionHTML
+            url
+            stargazerCount
+            primaryLanguage {
+              color
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+  `;
+
+const { loading, error, data } = useQuery(GET_GITHUB);
+if (data) {console.log(data)};
+if (error) {console.log(error)};
+
+
   return (
     <>
       <Header />
@@ -12,7 +49,7 @@ function App() {
         <div className="col-span-1 flex items-center">
           <>
             <Sidebar />
-            <Seperator vertical={true}/>
+            <Seperator vertical={true} />
           </>
         </div>
         <div className="col-span-3">
--- github-repository-search/src/index.tsx
+++ github-repository-search/src/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  createHttpLink,
+  ApolloLink,
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
 
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+const authLink = new ApolloLink((operation, forward) => {
+  // TODO: add bearer token support
+  operation.setContext({
+    headers: {
+      Authorization: `Bearer `,
+    },
+  });
+  console.log(process.env.GITHUB_TOKEN)
+  return forward(operation);
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
